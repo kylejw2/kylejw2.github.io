48609ee6dab8602be97c52d4b250cd9068c48dc2
Kyle Wright <kylejamwright@gmail.com>
2020-09-22 22:12:42 -0600

    Updates

---
+++ precache-manifest.8b1a8d7f0b77d273d2895786929b51d8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63f5fc5869609f5424f3a72c9c6aff82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b954186206b0198e5085",
+    "url": "/static/css/main.ceed2c8e.chunk.css"
+  },
+  {
+    "revision": "e20ad16b7b005e170722",
+    "url": "/static/js/2.cd9e3c66.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cd9e3c66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b954186206b0198e5085",
+    "url": "/static/js/main.a7ec734d.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "fa2ac78692bb5b74d40f9a0f2b0e55ef",
+    "url": "/static/media/Cool.fa2ac786.png"
+  },
+  {
+    "revision": "531553d1073a8e995776f4859dfa9230",
+    "url": "/static/media/Event-Calendar.531553d1.jpg"
+  },
+  {
+    "revision": "fa9254196b9ea465bbfff2803128ce4d",
+    "url": "/static/media/Glossary.fa925419.jpg"
+  },
+  {
+    "revision": "1695685f865177fd53ec4f4a9c05db78",
+    "url": "/static/media/KyleWrightResume.1695685f.pdf"
+  },
+  {
+    "revision": "dd2f83e38158b1ffa1f024a9da8473b3",
+    "url": "/static/media/Laborh.dd2f83e3.jpg"
+  },
+  {
+    "revision": "9d67967241b7380950fd4a13db437174",
+    "url": "/static/media/Mars.9d679672.png"
+  },
+  {
+    "revision": "2334ee6828c14eb56c05968ab49f8fc8",
+    "url": "/static/media/Portfolio.2334ee68.jpg"
+  },
+  {
+    "revision": "537932bc102f7aee31b783adfa0b0e3e",
+    "url": "/static/media/Yedding.537932bc.jpg"
+  },
+  {
+    "revision": "f1b96aada8224efca9e6f664d7ff770c",
+    "url": "/static/media/sun.f1b96aad.png"
+  }
+]);
--- src/components/Redirect.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Redirect = props => {
-    props.history.push('/Home');
-
-    return (<div></div>)
-}
-
-export default Redirect;
